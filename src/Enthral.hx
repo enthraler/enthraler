3558478767059bc87d89a9e5c43e60720fbb7f32
Jason O'Neil <jason.oneil@gmail.com>
2016-12-11 10:01:59 +1100

    Move more logic into SystemJs externs.

--- src/Enthral.hx
+++ src/Enthral.hx
@@ -6,19 +6,9 @@ class Enthral {
 	static function main() {
 		var cont = Browser.document.getElementById('container');
 		var authorData = {name: 'Jason'};
-		SystemJs.importJs('component.js').then(function (componentCls:Dynamic) {
-			var component = newComponent(componentCls, authorData);
+		SystemJs.importJs('component.js').then(function (componentCls:Module) {
+			var component:StaticComponent<{name:String}> = componentCls.instantiate(authorData);
 			component.setupView(cont);
 		});
-	}
-
-	/**
-		Treat a dynamic value as a component class and instantiate it with author data.
-
-		We need this because Haxe does not want to accept `new dynamicValue()` as valid syntax.
-	**/
-	static function newComponent<AuthorData>(cls:Dynamic, authorData:AuthorData):StaticComponent<AuthorData> {
-		// TODO: check for errors, and probably check that it is in fact a component as well.
-		return untyped __js__('new cls(authorData)');
 	}
 }
--- src/SystemJs.hx
+++ src/SystemJs.hx
@@ -4,8 +4,56 @@ typedef SystemJsConfig = {
 	?baseURL:String
 };
 
+abstract Module(Dynamic) from Dynamic {
+	inline public function new(module:Dynamic) {
+		this = module;
+	}
+
+	inline public function instantiate(?arg1:Dynamic, ?arg2:Dynamic, ?arg3:Dynamic, ?arg4:Dynamic, ?arg5:Dynamic) {
+		return inst(this, arg1, arg2, arg3, arg4, arg5);
+	}
+
+	static function inst(mod:Class<Dynamic>, arg1, arg2, arg3, arg4, arg5) {
+		return untyped __js__('new mod(arg1, arg2, arg3, arg4, arg5)');
+	}
+}
+
+/**
+	API: https://github.com/systemjs/systemjs/blob/master/docs/system-api.md
+**/
 @:native('SystemJS')
 extern class SystemJs {
+	public static function amdDefine():Void;
+
+	public static function amdRequire():Void;
+
 	public static function config(conf:SystemJsConfig):Void;
-	@:native('import') public static function importJs(file:String):Promise<Dynamic>;
+
+	/**
+		Instantiates a copy of SystemJS with different options.
+		To reflect this properly in Haxe we would need both static and instance functions with the same names.
+	**/
+	public static function constructor(conf:SystemJsConfig):Class<SystemJs>;
+
+	public static function delete(moduleName:String):Bool;
+
+	public static function get(moduleName:String):Module;
+
+	public static function has(moduleName:String):Bool;
+
+	@:native('import')
+	@:overload(function (file:String, normalisedParentName:String):Promise<Module> {})
+	public static function importJs(file:String):Promise<Module>;
+
+	public static function newModule(jsObject:Dynamic):Module;
+
+	@:overload(function (dependencies:Array<String>, executingRequire:Bool, declare:Dynamic->Void):Void {})
+	public static function register(name:String, dependencies:Array<String>, declare:Dynamic->Void):Void;
+
+	@:overload(function (dependencies:Array<String>, executingRequire:Bool, declare:Dynamic->Dynamic->Dynamic->Void):Void {})
+	public static function registerDynamic(name:String, dependencies:Array<String>, executingRequire:Bool, declare:Dynamic->Dynamic->Dynamic->Void):Void;
+
+	public static function set(moduleName:String, module:Module):Void;
+
+	public static function _nodeRequire():Void;
 }
