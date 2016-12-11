ba7c23cfbe25d037bda4802ac739520514ccb637
Jason O'Neil <jason.oneil@gmail.com>
2016-12-11 11:22:16 +1100

    Read enthral script and props from DOM element.

--- src/Enthral.hx
+++ src/Enthral.hx
@@ -8,11 +8,17 @@ class Enthral {
 	static function main() {
 		var enthral = new Enthral();
 		var cont = Browser.document.getElementById('container');
-		var authorData = {name: 'Jason'};
-		enthral.createEmbed('component.js', authorData, cont);
+		enthral.transformElement(cont);
 	}
 
 	public function new() {}
+
+	public function transformElement(elm:Element) {
+		var script = elm.attributes.getNamedItem('data-enthral-script').value;
+		var props = elm.attributes.getNamedItem('data-enthral-props').value;
+		var data = haxe.Json.parse(props);
+		return createEmbed(script, data, elm);
+	}
 
 	public function createEmbed<T>(componentScript:String, componentData:T, container:Element):Promise<StaticComponent<T>> {
 		return SystemJs.importJs(componentScript).then(function (componentCls:Module) {
