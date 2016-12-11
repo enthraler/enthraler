623715cda6e2ab7500de2d2c77844c7bad354f55
Jason O'Neil <jason.oneil@gmail.com>
2016-12-12 07:19:52 +1100

    Load props from JSON request.
    So far so good!
    
    - The enthral.js file has:
        - no understanding of the "hello" component
        - no knowledge of either hello-anna.json or hello-jason.json
    - It searches for embed components
    - When it finds them
        - it loads the script for that type of component
        - if the script is already loaded, it doesn't need to reload it.
        - it loads the data from the JSON file
        - it renders the component

--- src/Enthral.hx
+++ src/Enthral.hx
@@ -26,12 +26,17 @@ class Enthral {
 	public function setupComponent(elm:Element) {
 		var script = elm.attributes.getNamedItem('data-enthral-script').value;
 		var props = elm.attributes.getNamedItem('data-enthral-props').value;
-		var data = haxe.Json.parse(props);
-		return instantiateComponent(script, data, elm);
+		return instantiateComponent(script, props, elm);
 	}
 
-	public function instantiateComponent<T>(componentScript:String, componentData:T, container:Element):Promise<StaticComponent<T>> {
-		return SystemJs.importJs(componentScript).then(function (componentCls:Module) {
+	public function instantiateComponent<T>(componentScriptUrl:String, componentDataUrl:String, container:Element):Promise<StaticComponent<T>> {
+		var scriptPromise = SystemJs.importJs(componentScriptUrl);
+		var dataPromise = Browser.window.fetch(componentDataUrl).then(function (resp) {
+			return resp.json();
+		});
+		return Promise.all([scriptPromise, dataPromise]).then(function (arr:Array<Dynamic>) {
+			var componentCls:Module = arr[0];
+			var componentData:T = arr[1];
 			var component:StaticComponent<T> = componentCls.instantiate(componentData);
 			component.setupView(container);
 			return component;
