dc2c6bf9ca61dbb278971bf22c48afc2026fe9d0
Jason O'Neil <jason.oneil@gmail.com>
2016-12-13 08:18:30 +1100

    Allow loading external dependencies
    - an "enthralDependencies" static value on the class/function
    - each field on the object is the dependency name, each value it's URL (this may change)
    - these are loaded through SystemJS and passed as an object to the component
    - the new component constructor is `new MyComponent(authorData, deps:{myDep:MyDep, "jQuery":JQuery})`

--- src/Enthral.hx
+++ src/Enthral.hx
@@ -30,16 +30,41 @@ class Enthral {
 	}
 
 	public function instantiateComponent<T>(componentScriptUrl:String, componentDataUrl:String, container:Element):Promise<StaticComponent<T>> {
-		var scriptPromise = SystemJs.importJs(componentScriptUrl);
+		var scriptPromise = SystemJs.importJs(componentScriptUrl).then(loadModuleDependencies);
 		var dataPromise = Browser.window.fetch(componentDataUrl).then(function (resp) {
 			return resp.json();
 		});
 		return Promise.all([scriptPromise, dataPromise]).then(function (arr:Array<Dynamic>) {
 			var componentCls:Module = arr[0];
 			var componentData:T = arr[1];
-			var component:StaticComponent<T> = componentCls.instantiate(componentData);
+			var dependencies = (componentCls:Dynamic).enthralLoadedDependencies;
+			var component:StaticComponent<T> = componentCls.instantiate(componentData, dependencies);
 			component.setupView(container);
 			return component;
+		});
+	}
+
+	/**
+		Given a loaded module, see if an `enthralDependencies` array exists.
+		If it does, load each of the dependencies, and return a promise for once they are ready to go.
+	**/
+	function loadModuleDependencies(module:Dynamic):Promise<Module> {
+		// TODO: use an abstract here instead of Dynamic and Reflect. ObjectMap isn't quite what we need either.
+		var loadedDependencies:Dynamic<Module> = {},
+			promises = [];
+		if (module.enthralDependencies != null) {
+			var deps:Dynamic<String> = module.enthralDependencies;
+			for (name in Reflect.fields(deps)) {
+				var url = Reflect.field(deps, name);
+				var depLoaded = SystemJs.importJs(url).then(function (dep:Module) {
+					Reflect.setField(loadedDependencies, name, dep);
+				});
+				promises.push(depLoaded);
+			}
+		}
+		return Promise.all(promises).then(function (_) {
+			module.enthralLoadedDependencies = loadedDependencies;
+			return module;
 		});
 	}
 }
--- src/examplecomponents/Hello.hx
+++ src/examplecomponents/Hello.hx
@@ -8,11 +8,19 @@ class Hello implements StaticComponent<{name:String}> {
 	public var authorData:{name:String};
 	public var meta:ComponentMeta;
 
-	public function new(authorData) {
+	public function new(authorData, deps) {
 		this.authorData = authorData;
+		trace(deps.jquery("body"));
 	}
 
 	public function setupView(container:Element) {
 		container.innerHTML = 'Hello ${authorData.name}';
 	}
+
+	public static var enthralPropTypes = {
+		name:"String"
+	}
+	public static var enthralDependencies = {
+		jquery: "https://code.jquery.com/jquery-3.1.1.min.js"
+	};
 }
