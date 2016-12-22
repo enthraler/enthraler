94faf7e03c8de469107eea142550a895800d2c8a
Jason O'Neil <jason.oneil@gmail.com>
2016-12-22 09:39:58 +0800

    Add EnthralHost javascript file.
    Begin setting up cross-frame (and cross-domain) communication using window.postMessage.

--- src/EnthralFrame.hx
+++ src/EnthralFrame.hx
@@ -1,16 +1,23 @@
-import js.Browser;
+import js.Browser.document;
+import js.Browser.window;
+import js.html.*;
 import enthral.Enthral;
 
 class EnthralFrame {
 	public static function main() {
 		var params = getParamsFromLocation();
+		addMessageListener();
+		loadEnthralEmbed(params);
+	}
+
+	static function loadEnthralEmbed(params:Map<String,String>) {
 		var enthral = new Enthral();
-		var container = Browser.document.getElementById('container');
+		var container = document.getElementById('container');
 		enthral.instantiateComponent(params['script'], params['props'], container);
 	}
 
 	static function getParamsFromLocation() {
-		var hash = Browser.location.hash;
+		var hash = window.location.hash;
 		var paramStrings = hash.substr(hash.indexOf('?') + 1).split('&');
 		var params = new Map();
 		for (str in paramStrings) {
@@ -18,5 +25,26 @@ class EnthralFrame {
 			params[parts[0]] = parts[1];
 		}
 		return params;
+	}
+
+	static function addMessageListener() {
+		window.addEventListener('message', function (e:MessageEvent) {
+			var host:Window = e.source;
+			var message = e.data;
+			var origin = e.origin;
+
+			if (message.type == 'init') {
+				var reply = {
+					'type': 'register',
+					'url': window.location.hash
+				};
+				trace('Connected to host:', host);
+				// TODO: use the host origin.
+				host.postMessage(reply, '*');
+			} else {
+				// Check the origin!
+				trace('Is origin correct?', origin);
+			}
+		});
 	}
 }
---
+++ src/EnthralHost.hx
@@ -0,0 +1,49 @@
+import js.Browser.window;
+import js.Browser.document;
+import js.html.*;
+
+class EnthralHost {
+	static function main() {
+		if (document.readyState != 'loading') {
+			init();
+		} else {
+			document.addEventListener('DOMContentLoaded', init);
+		}
+	}
+
+	static function init() {
+		addMessageListener();
+		initAllFrames();
+	}
+
+	static function initAllFrames() {
+		var frames = document.querySelectorAll('iframe.enthral-embed');
+		for (elm in frames) {
+			var iframe:IFrameElement = cast elm;
+			// We don't have a way to check if an iFrame on a 3rd party domain is loaded already.
+			// To be safe, we send a message immediately, as well as on a 'load' event.
+			initFrame(iframe);
+			iframe.addEventListener('load', function (e) {
+				initFrame(iframe);
+			});
+		}
+	}
+
+	static function initFrame(iframe:IFrameElement) {
+		var message = {
+			type: 'init',
+			greeting: 'Hello Embed, I am Host!'
+		};
+		// TODO: specify an origin based on iframe.src, rather than using a wildcard.
+		iframe.contentWindow.postMessage(message, '*');
+	}
+
+	static function addMessageListener() {
+		window.addEventListener('message', function (e:MessageEvent) {
+			var frame:Window = e.source;
+			var message = e.data;
+			var origin = e.origin;
+			trace('Received message from frame', frame, message);
+		});
+	}
+}
