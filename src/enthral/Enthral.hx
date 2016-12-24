296241799dac0a624da41f7d0feec389bf4586b2
Jason O'Neil <jason.oneil@gmail.com>
2016-12-24 14:00:01 +0800

    Add special `authorData.enthral` property.
    We'll use this to make the dataPath URL available to our components,
    so that they can load files in paths relative to their JSON file.
    And als the scriptPath URL, so they can load files relative to their JS file.

--- src/enthral/Enthral.hx
+++ src/enthral/Enthral.hx
@@ -6,26 +6,26 @@ import js.html.*;
 import enthral.Component;
 import enthral.PropTypes;
 import SystemJs;
+using haxe.io.Path;
 
 class Enthral {
 	public function new() {}
 
 	public function instantiateComponent<T>(componentScriptUrl:String, componentDataUrl:String, container:Element):Promise<StaticComponent<T>> {
-		var scriptPromise = SystemJs.importJs(componentScriptUrl).then(loadModuleDependencies);
-		var dataPromise = Browser.window.fetch(componentDataUrl).then(function (resp) {
-			return resp.json();
-		});
+		var scriptPromise = SystemJs.importJs(componentScriptUrl).then(loadModuleDependencies),
+			dataPromise = Browser.window.fetch(componentDataUrl).then(function (resp:Response) {
+				return resp.json().then(function (data) {
+					data.enthral = {
+						scriptUrl: componentScriptUrl,
+						scriptPath: componentScriptUrl.directory().addTrailingSlash(),
+						dataUrl: componentDataUrl,
+						dataPath: componentDataUrl.directory().addTrailingSlash()
+					};
+					return data;
+				});
+			});
 		return Promise.all([scriptPromise, dataPromise]).then(function (arr:Array<Dynamic>) {
-			var componentCls:Module = arr[0];
-			var componentData:T = arr[1];
-			var dependencies = (componentCls:Dynamic).enthralLoadedDependencies;
-			var schema = (componentCls:Dynamic).enthralPropTypes;
-			if (schema != null) {
-				PropTypes.validate(schema, componentData, componentDataUrl);
-			}
-			var component:StaticComponent<T> = componentCls.instantiate(componentData, dependencies);
-			component.setupView(container);
-			return component;
+			return mountComponent(arr[0], arr[1], container);
 		});
 	}
 
@@ -51,5 +51,18 @@ class Enthral {
 			module.enthralLoadedDependencies = loadedDependencies;
 			return module;
 		});
+	}
+
+	function mountComponent<T>(componentCls:Module, componentData:T, container:Element):StaticComponent<T> {
+		var dependencies = (componentCls:Dynamic).enthralLoadedDependencies,
+			schema = (componentCls:Dynamic).enthralPropTypes,
+			enthralData = (componentData:Dynamic).enthral;
+		if (schema != null) {
+			PropTypes.validate(schema, componentData, enthralData.dataUrl);
+		}
+
+		var component:StaticComponent<T> = componentCls.instantiate(componentData, dependencies);
+		component.setupView(container);
+		return component;
 	}
 }
