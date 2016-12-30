f889b29315668b36d0ceac7bf592c2a960db97db
Jason O'Neil <jason.oneil@gmail.com>
2016-12-30 16:55:45 +0800

    No longer use `enthralDependencies` magic property.
    Just use System JS loader instead.  They can use common JS or AMD or
    whatever to import their dependencies.

--- src/enthral/Enthral.hx
+++ src/enthral/Enthral.hx
@@ -37,7 +37,7 @@ class Enthral {
 	}
 
 	public function instantiateComponent<T>(componentScriptUrl:String, componentDataUrl:String, container:Element):Promise<StaticComponent<T>> {
-		var scriptPromise = SystemJs.importJs(componentScriptUrl).then(loadModuleDependencies),
+		var scriptPromise = SystemJs.importJs(componentScriptUrl),
 			dataPromise = Browser.window.fetch(componentDataUrl).then(function (resp:Response) {
 				return resp.json().then(function (data) {
 					data.enthral = {
@@ -51,30 +51,6 @@ class Enthral {
 			});
 		return Promise.all([scriptPromise, dataPromise]).then(function (arr:Array<Dynamic>) {
 			return mountComponent(arr[0], arr[1], container);
-		});
-	}
-
-	/**
-		Given a loaded module, see if an `enthralDependencies` object exists.
-		If it does, load each of the dependencies, and return a promise for once they are ready to go.
-	**/
-	function loadModuleDependencies(module:Dynamic):Promise<Module> {
-		// TODO: use an abstract here instead of Dynamic and Reflect. ObjectMap isn't quite what we need either.
-		var loadedDependencies:Dynamic<Module> = {},
-			promises = [];
-		if (module.enthralDependencies != null) {
-			var deps:Dynamic<String> = module.enthralDependencies;
-			for (name in Reflect.fields(deps)) {
-				var url = Reflect.field(deps, name);
-				var depLoaded = SystemJs.importJs(url).then(function (dep:Module) {
-					Reflect.setField(loadedDependencies, name, dep);
-				});
-				promises.push(depLoaded);
-			}
-		}
-		return Promise.all(promises).then(function (_) {
-			module.enthralLoadedDependencies = loadedDependencies;
-			return module;
 		});
 	}
 
