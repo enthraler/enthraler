6fc4ebe940b96009feb051a4bfdfba322799d942
Jason O'Neil <jason.oneil@gmail.com>
2017-02-20 22:17:28 +1100

    Rename HaxeComponent -> HaxeEnthralerTemplate

--- src/enthraler/HaxeEnthralerTemplate.hx
+++ src/enthraler/HaxeEnthralerTemplate.hx
@@ -3,7 +3,7 @@ package enthraler;
 /**
 	If you are building an enthraler template in Haxe, have it implement this interface.
 
-	It will run a build macro to define the template using AMD, set up extern dependencies correctly, and check the types.
+	It will run a build macro to define the template using AMD, set up extern dependencies correctly, and (TODO) check the types match our typedef.
 **/
 @:autoBuild(enthraler.macro.HaxeEnthralerTemplateBuilder.build())
 interface HaxeEnthralerTemplate<Props> {
