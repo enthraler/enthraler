5a1083d8553bb88277beb2caf1108595520a7ad7
Jason O'Neil <jason.oneil@gmail.com>
2017-06-25 20:21:19 +1000

    Add functions for generating UUIDs

--- src/enthralerdotcom/types/ContentGuid.hx
+++ src/enthralerdotcom/types/ContentGuid.hx
@@ -1,9 +1,14 @@
 package enthralerdotcom.types;
 
 import sys.db.Types;
+import enthralerdotcom.util.Uuid;
 
 abstract ContentGuid(SString<36>) to String {
 	public function new(guid:String) {
 		this = guid;
+	}
+
+	public static function generate() {
+		return new ContentGuid(Uuid.generate());
 	}
 }
